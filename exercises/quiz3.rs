c50ababef45c8af2eb9f446a1181cb8bca16ef31
root <root@AB.localdomain>
2025-04-02 21:50:48 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,16 +18,25 @@
 
 // I AM NOT DONE
 
+enum Grade {
+    Numeric(f32),
+    Letter(String),
+}
+
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
+        let grade_display = match &self.grade {
+            Grade::Numeric(n) => n.to_string(),
+            Grade::Letter(l) => l.clone(),
+        };
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+                &self.student_name, &self.student_age, grade_display)
     }
 }
 
@@ -38,7 +47,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,9 +59,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Letter("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
