3d80424dafeec8d6481a388262a881a42d96cf11
root <root@AB.localdomain>
2025-04-06 18:29:32 +0800

    update

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -7,7 +7,6 @@
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
-
 #[derive(Debug)]
 struct TreeNode<T>
 where
@@ -37,39 +36,56 @@ where
             right: None,
         }
     }
+
+    // 合并后的insert实现
+    fn insert(&mut self, value: T) {
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                if let Some(ref mut left) = self.left {
+                    left.insert(value);
+                } else {
+                    self.left = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Greater => {
+                if let Some(ref mut right) = self.right {
+                    right.insert(value);
+                } else {
+                    self.right = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Equal => {}
+        }
+    }
+
+    fn search(&self, value: T) -> bool {
+        match value.cmp(&self.value) {
+            Ordering::Equal => true,
+            Ordering::Less => self.left.as_ref().map_or(false, |l| l.search(value)),
+            Ordering::Greater => self.right.as_ref().map_or(false, |r| r.search(value)),
+        }
+    }
 }
 
 impl<T> BinarySearchTree<T>
 where
     T: Ord,
 {
-
     fn new() -> Self {
         BinarySearchTree { root: None }
     }
 
-    // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root {
+            Some(ref mut node) => node.insert(value),
+            None => self.root = Some(Box::new(TreeNode::new(value))),
+        }
     }
 
-    // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        self.root.as_ref().map_or(false, |n| n.search(value))
     }
 }
-
-impl<T> TreeNode<T>
-where
-    T: Ord,
-{
-    // Insert a node into the tree
-    fn insert(&mut self, value: T) {
-        //TODO
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
@@ -78,49 +94,40 @@ mod tests {
     #[test]
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
+        assert!(!bst.search(1));
 
-        
-        assert_eq!(bst.search(1), false);
-
-        
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
-        assert_eq!(bst.search(5), true);
-        assert_eq!(bst.search(3), true);
-        assert_eq!(bst.search(7), true);
-        assert_eq!(bst.search(2), true);
-        assert_eq!(bst.search(4), true);
+        assert!(bst.search(5));
+        assert!(bst.search(3));
+        assert!(bst.search(7));
+        assert!(bst.search(2));
+        assert!(bst.search(4));
 
-        
-        assert_eq!(bst.search(1), false);
-        assert_eq!(bst.search(6), false);
+        assert!(!bst.search(1));
+        assert!(!bst.search(6));
     }
 
     #[test]
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
-
-        
         bst.insert(1);
         bst.insert(1);
 
-        
-        assert_eq!(bst.search(1), true);
+        assert!(bst.search(1));
 
-        
-        match bst.root {
-            Some(ref node) => {
-                assert!(node.left.is_none());
-                assert!(node.right.is_none());
-            },
-            None => panic!("Root should not be None after insertion"),
+        if let Some(ref node) = bst.root {
+            assert!(node.left.is_none());
+            assert!(node.right.is_none());
+        } else {
+            panic!("Root should not be None");
         }
     }
-}    
+}
+
 
 
