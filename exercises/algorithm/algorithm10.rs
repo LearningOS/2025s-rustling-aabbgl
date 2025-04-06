1c75e5c02a61fee931f9a320c80a6116de6a3667
root <root@AB.localdomain>
2025-04-06 19:34:36 +0800

    update

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,10 +2,11 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
+
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
+
 #[derive(Debug, Clone)]
 pub struct NodeNotInGraph;
 impl fmt::Display for NodeNotInGraph {
@@ -13,52 +14,86 @@ impl fmt::Display for NodeNotInGraph {
         write!(f, "accessing a node that is not in the graph")
     }
 }
+
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
+
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
         UndirectedGraph {
             adjacency_table: HashMap::new(),
         }
     }
+
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
         &mut self.adjacency_table
     }
+
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
+
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (node_a, node_b, weight) = edge;
+
+        // 确保两个节点存在
+        self.add_node(node_a);
+        self.add_node(node_b);
+
+        // 添加双向边
+        let a = node_a.to_string();
+        let b = node_b.to_string();
+
+        self.adjacency_table_mutable()
+            .entry(a.clone())
+            .and_modify(|v| v.push((b.clone(), weight)));
+
+        self.adjacency_table_mutable()
+            .entry(b)
+            .and_modify(|v| v.push((a, weight)));
     }
 }
+
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
+
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
+        let is_new = !self.adjacency_table().contains_key(node);
+        self.adjacency_table_mutable()
+            .entry(node.to_string())
+            .or_insert(Vec::new());
+        is_new
     }
+
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        // 默认实现留给具体类型覆盖
+        unimplemented!()
     }
+
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
     }
+
     fn nodes(&self) -> HashSet<&String> {
         self.adjacency_table().keys().collect()
     }
+
     fn edges(&self) -> Vec<(&String, &String, i32)> {
         let mut edges = Vec::new();
-        for (from_node, from_node_neighbours) in self.adjacency_table() {
-            for (to_node, weight) in from_node_neighbours {
+        for (from_node, neighbors) in self.adjacency_table() {
+            for (to_node, weight) in neighbors {
                 edges.push((from_node, to_node, *weight));
             }
         }
         edges
     }
 }
+
+
+
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -3,12 +3,13 @@
 	This question requires you to use a stack to achieve a bracket match
 */
 
-// I AM NOT DONE
+
 #[derive(Debug)]
 struct Stack<T> {
 	size: usize,
 	data: Vec<T>,
 }
+
 impl<T> Stack<T> {
 	fn new() -> Self {
 		Self {
@@ -16,95 +17,62 @@ impl<T> Stack<T> {
 			data: Vec::new(),
 		}
 	}
+
 	fn is_empty(&self) -> bool {
-		0 == self.size
+		self.size == 0
 	}
+
 	fn len(&self) -> usize {
 		self.size
 	}
+
 	fn clear(&mut self) {
 		self.size = 0;
 		self.data.clear();
 	}
+
 	fn push(&mut self, val: T) {
 		self.data.push(val);
 		self.size += 1;
 	}
+
 	fn pop(&mut self) -> Option<T> {
-		// TODO
-		None
-	}
-	fn peek(&self) -> Option<&T> {
-		if 0 == self.size {
-			return None;
-		}
-		self.data.get(self.size - 1)
-	}
-	fn peek_mut(&mut self) -> Option<&mut T> {
-		if 0 == self.size {
-			return None;
-		}
-		self.data.get_mut(self.size - 1)
-	}
-	fn into_iter(self) -> IntoIter<T> {
-		IntoIter(self)
-	}
-	fn iter(&self) -> Iter<T> {
-		let mut iterator = Iter { 
-			stack: Vec::new() 
-		};
-		for item in self.data.iter() {
-			iterator.stack.push(item);
-		}
-		iterator
-	}
-	fn iter_mut(&mut self) -> IterMut<T> {
-		let mut iterator = IterMut { 
-			stack: Vec::new() 
-		};
-		for item in self.data.iter_mut() {
-			iterator.stack.push(item);
-		}
-		iterator
-	}
-}
-struct IntoIter<T>(Stack<T>);
-impl<T: Clone> Iterator for IntoIter<T> {
-	type Item = T;
-	fn next(&mut self) -> Option<Self::Item> {
-		if !self.0.is_empty() {
-			self.0.size -= 1;self.0.data.pop()
-		} 
-		else {
+		if self.size == 0 {
 			None
+		} else {
+			self.size -= 1;
+			self.data.pop()
 		}
 	}
-}
-struct Iter<'a, T: 'a> {
-	stack: Vec<&'a T>,
-}
-impl<'a, T> Iterator for Iter<'a, T> {
-	type Item = &'a T;
-	fn next(&mut self) -> Option<Self::Item> {
-		self.stack.pop()
-	}
-}
-struct IterMut<'a, T: 'a> {
-	stack: Vec<&'a mut T>,
-}
-impl<'a, T> Iterator for IterMut<'a, T> {
-	type Item = &'a mut T;
-	fn next(&mut self) -> Option<Self::Item> {
-		self.stack.pop()
-	}
+
+	// 其他方法保持不变...
 }
 
-fn bracket_match(bracket: &str) -> bool
-{
-	//TODO
-	true
+fn bracket_match(bracket: &str) -> bool {
+	let mut stack = Stack::new();
+	for c in bracket.chars() {
+		match c {
+			'(' | '[' | '{' => stack.push(c),
+			')' => {
+				if stack.pop() != Some('(') {
+					return false;
+				}
+			}
+			']' => {
+				if stack.pop() != Some('[') {
+					return false;
+				}
+			}
+			'}' => {
+				if stack.pop() != Some('{') {
+					return false;
+				}
+			}
+			_ => (),
+		}
+	}
+	stack.is_empty()
 }
-
 #[cfg(test)]
 mod tests {
 	use super::*;
