dedf1b9f4fba74a0e2e3b99e2dced7d26927c58a
root <root@AB.localdomain>
2025-04-06 17:54:59 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -30,13 +30,17 @@
 // I AM NOT DONE
 
 extern "Rust" {
+    #[link_name = "my_demo_function"]
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
+
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
+    #[export_name = "my_demo_function"]
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    pub fn my_demo_function(a: u32) -> u32 {
         a
     }
 }
